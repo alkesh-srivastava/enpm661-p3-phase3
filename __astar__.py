a2bd739f76a8d98bf5036962f2ffd8cf4e74b225
alkesh-umd <78883501+alkesh-umd@users.noreply.github.com>
2021-04-16 18:24:21 -0400

    Update __astar__.py
    Added lrpm, rrpm

--- __astar__.py
+++ __astar__.py
@@ -7,8 +7,9 @@ import math, cv2
 def heuristic(a, b):
     # Euclidean Distance
     x1, y1, theta1 = a
-    x2, y2, theta2 = b
+    x2, y2, theta2, lrpm1, rrpm1 = b
     return abs(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
+
 
 def cv2_viz(visited_nodes, path, clearance):
     canvas = cv2.imread("Map.png")
@@ -21,7 +22,8 @@ def cv2_viz(visited_nodes, path, clearance):
     while True:
         cv2.arrowedLine(canvas, (int(visited_nodes[i][0]), 300 - int(visited_nodes[i][1])),
                         (int(visited_nodes[i + 1][0]), 300 - int(visited_nodes[i + 1][1])), (255, 0, 0), 1, cv2.LINE_AA)
-        canvas = cv2.putText(canvas, "Robot Cearance : " + str(clearance) + " units", (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+        canvas = cv2.putText(canvas, "Robot Cearance : " + str(clearance) + " units", (50, 50), cv2.FONT_HERSHEY_PLAIN,
+                             1, (0, 0, 255), 2)
         out.write(canvas)
         i += 1
         if i == (len(visited_nodes) - 1):
@@ -40,6 +42,7 @@ def cv2_viz(visited_nodes, path, clearance):
         cv2.imshow("Solution", canvas)
         cv2.waitKey(1)
 
+
 def a_star(maze, start, goal, rpm_1, rpm_2):
     pq = PriorityQueue()
     pq.put(start, 0)
@@ -47,7 +50,7 @@ def a_star(maze, start, goal, rpm_1, rpm_2):
     g_values = {start: 0}
     while not pq.is_empty():
         current_cell = pq.get()
-        if((current_cell[0] - goal[0]) ** 2) + ((current_cell[1] - goal[1]) ** 2) - 1 <= 0:
+        if ((current_cell[0] - goal[0]) ** 2) + ((current_cell[1] - goal[1]) ** 2) - 1 <= 0:
             return return_path(predecessors, start, current_cell)
         for direction in [
             offsets_determination(0, rpm_1, current_cell[2]),
@@ -61,17 +64,23 @@ def a_star(maze, start, goal, rpm_1, rpm_2):
         ]:
             row_offset, col_offset, theta_offset, left_rpm, right_rpm = direction
             if 0 <= (current_cell[2] + theta_offset) < 360:
-                neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset, current_cell[2] + theta_offset)
+                neighbour = (
+                current_cell[0] + row_offset, current_cell[1] + col_offset, current_cell[2] + theta_offset, left_rpm,
+                right_rpm)
             elif current_cell[2] + theta_offset > 360:
-                neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset, (current_cell[2] + theta_offset) - 360)
+                neighbour = (
+                current_cell[0] + row_offset, current_cell[1] + col_offset, (current_cell[2] + theta_offset) - 360,
+                left_rpm, right_rpm)
             elif current_cell[2] + theta_offset == 360:
                 neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset, 0)
-            elif (current_cell[2] + theta_offset) < 0 :
-                neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset, 360 + (current_cell[2] + theta_offset))
+            elif (current_cell[2] + theta_offset) < 0:
+                neighbour = (
+                current_cell[0] + row_offset, current_cell[1] + col_offset, 360 + (current_cell[2] + theta_offset),
+                left_rpm, right_rpm)
             # Xi,Yi,Thetai,UL,UR
             if move_possible(maze, neighbour) and neighbour not in g_values:
                 # delta_cost = math.sqrt((direction[0]**2) + (direction[1]**2))
-                _, _, _, delta_cost = cost(current_cell[0],current_cell[1], current_cell[2],left_rpm, right_rpm)
+                _, _, _, delta_cost = cost(current_cell[0], current_cell[1], current_cell[2], left_rpm, right_rpm)
                 new_cost = g_values[current_cell] + delta_cost
                 g_values[neighbour] = new_cost
                 f_value = new_cost + heuristic(goal, neighbour)
