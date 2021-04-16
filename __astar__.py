ec8ab777ad8a6d144853d37a3af45124d6d92787
alkesh-umd <78883501+alkesh-umd@users.noreply.github.com>
2021-04-16 19:14:40 -0400

    Update __astar__.py
    Explored

--- __astar__.py
+++ __astar__.py
@@ -90,3 +90,55 @@ def a_star(maze, start, goal, rpm_1, rpm_2):
 
     return None
 # A -star co-ordinate of the next
+
+
+# Explored :
+def a_star_explore(maze, start, goal, rpm_1, rpm_2):
+    explored_list = list()
+
+    pq = PriorityQueue()
+    pq.put(start, 0)
+    predecessors = {start: None}
+    g_values = {start: 0}
+    while not pq.is_empty():
+        current_cell = pq.get()
+        if ((current_cell[0] - goal[0]) ** 2) + ((current_cell[1] - goal[1]) ** 2) - 1 <= 0:
+            return explored_list
+        for direction in [
+            offsets_determination(0, rpm_1, current_cell[2]),
+            offsets_determination(rpm_1, 0, current_cell[2]),
+            offsets_determination(rpm_1, rpm_1, current_cell[2]),
+            offsets_determination(0, rpm_2, current_cell[2]),
+            offsets_determination(rpm_2, 0, current_cell[2]),
+            offsets_determination(rpm_2, rpm_2, current_cell[2]),
+            offsets_determination(rpm_1, rpm_2, current_cell[2]),
+            offsets_determination(rpm_2, rpm_1, current_cell[2])
+        ]:
+            row_offset, col_offset, theta_offset, left_rpm, right_rpm = direction
+            if 0 <= (current_cell[2] + theta_offset) < 360:
+                neighbour = (
+                current_cell[0] + row_offset, current_cell[1] + col_offset, current_cell[2] + theta_offset, left_rpm,
+                right_rpm)
+            elif current_cell[2] + theta_offset > 360:
+                neighbour = (
+                current_cell[0] + row_offset, current_cell[1] + col_offset, (current_cell[2] + theta_offset) - 360,
+                left_rpm, right_rpm)
+            elif current_cell[2] + theta_offset == 360:
+                neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset, 0)
+            elif (current_cell[2] + theta_offset) < 0:
+                neighbour = (
+                current_cell[0] + row_offset, current_cell[1] + col_offset, 360 + (current_cell[2] + theta_offset),
+                left_rpm, right_rpm)
+            # Xi,Yi,Thetai,UL,UR
+            if move_possible(maze, neighbour) and neighbour not in g_values:
+                # delta_cost = math.sqrt((direction[0]**2) + (direction[1]**2))
+                _, _, _, delta_cost = cost(current_cell[0], current_cell[1], current_cell[2], left_rpm, right_rpm)
+                new_cost = g_values[current_cell] + delta_cost
+                g_values[neighbour] = new_cost
+                f_value = new_cost + heuristic(goal, neighbour)
+                pq.put(neighbour, f_value)
+                predecessors[neighbour] = current_cell
+                print("Visited Node:", current_cell)
+                explored_list.append(neighbour)
+
+    return None
